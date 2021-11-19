7ac1ed48baad08e6cea0efdaf1063714c1ee3bfa
今天幸福不在家 <zhujwmail@163.com>
2021-11-19 17:56:30 +0800

    update

---
+++ base/src/main/java/com/zjw/regex/RegexExample2.java
@@ -0,0 +1,36 @@
+package com.zjw.regex;
+
+
+import org.junit.Test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author 朱俊伟
+ * @date 2021/11/16
+ */
+public class RegexExample2 {
+
+    @Test
+    public void testDemo(){
+        // 按指定模式在字符串查找
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(\\D*)(\\d+)(.*)";
+
+        // 创建 Pattern 对象
+        Pattern r = Pattern.compile(pattern);
+
+        // 现在创建 matcher 对象
+        Matcher m = r.matcher(line);
+        System.out.println(m.groupCount());
+        if (m.find( )) {
+            System.out.println("Found value: " + m.group(0) );
+            System.out.println("Found value: " + m.group(1) );
+            System.out.println("Found value: " + m.group(2) );
+            System.out.println("Found value: " + m.group(3) );
+        } else {
+            System.out.println("NO MATCH");
+        }
+    }
+}
---
+++ base/src/main/java/com/zjw/string/StringDemo.java
@@ -0,0 +1,31 @@
+package com.zjw.string;
+
+import com.sun.deploy.util.StringUtils;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author 朱俊伟
+ * @date 2021/11/16
+ */
+public class StringDemo {
+
+    //格式化输出
+    @Test
+    public void testStringFormat(){
+        System.out.println(String.format("默认为[%1$s]:",123));
+    }
+
+    //split
+
+    @Test
+    public void testStringSplit(){
+        String str = "abc,def,gh,i,jk";
+        //下面两个结果一样，为啥？
+        String[] strings = str.split(",");
+        String[] strings2 = str.split("\\,");
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(strings2));
+    }
+}
