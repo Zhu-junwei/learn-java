b29fe9a72e2124f95908972731e1e4c9b2b38950
今天幸福不在家 <zhujwmail@163.com>
2022-01-19 18:28:20 +0800

    Date after方法测试

--- base/src/main/java/com/zjw/time/TimeTest.java
+++ base/src/main/java/com/zjw/time/TimeTest.java
@@ -1,5 +1,8 @@
 package com.zjw.time;
 
+import org.junit.Test;
+
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -19,36 +22,58 @@ public class TimeTest {
     }
 
     public static void getTime(){
-        System.out.println(new Date().getTime());
+        System.out.println(System.currentTimeMillis());
     }
 
-    /*
+    /**
      * 将时间转换为时间戳
      */
     public static String dateToStamp(String time){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String stamp = "";
-        if (!"".equals(time)) {//时间不为空
+        //时间不为空
+        if (!"".equals(time)) {
             try {
                 stamp = String.valueOf(sdf.parse(time).getTime()/1000);
             } catch (Exception e) {
                 System.out.println("参数为空！");
             }
-        }else {    //时间为空
-            long current_time = System.currentTimeMillis();  //获取当前时间
-            stamp = String.valueOf(current_time/1000);
+            //时间为空
+        }else {
+            //获取当前时间
+            long currentTime = System.currentTimeMillis();
+            stamp = String.valueOf(currentTime/1000);
         }
         return stamp;
     }
 
 
-    /*
+    /**
      * 将时间戳转换为时间
      */
     public static String stampToDate(int time){
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        String time_Date = sdf.format(new Date(time * 1000L));
-        return time_Date;
+        String timeDate = sdf.format(new Date(time * 1000L));
+        return timeDate;
 
     }
+
+    /**
+     * public boolean after(Date when)
+     * 判断一个日期是否在另一个日期后面
+     * @throws ParseException
+     */
+    @Test
+    public void testDateAfter() throws ParseException {
+        Date date1 = new SimpleDateFormat("yyyy-MM-dd").parse("2022-02-03");
+        Date date2 = new SimpleDateFormat("yyyy-MM-dd").parse("2022-02-04");
+        //false
+        System.out.println(date1.after(date2));
+        //true
+        System.out.println(date2.after(date1));
+        //false
+        System.out.println(date1.after(date1));
+    }
+
+
 }
