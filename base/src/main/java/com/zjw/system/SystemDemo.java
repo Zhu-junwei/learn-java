c8410d04d7b43d5f4ce34b15bf90ce8d65937113
今天幸福不在家 <zhujwmail@163.com>
2022-01-20 13:50:36 +0800

    获取系统中的配置

---
+++ base/src/main/java/com/zjw/system/SystemDemo.java
@@ -0,0 +1,27 @@
+package com.zjw.system;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * @author 朱俊伟
+ * @date 2022/01/20
+ */
+public class SystemDemo {
+
+    public static void main(String[] args) {
+
+        //获取系统中的一些配置
+        Properties properties = System.getProperties();
+        Set<String> propertyNames = properties.stringPropertyNames();
+        for (String propertyName : propertyNames) {
+            System.out.println(propertyName + "--->" +properties.getProperty(propertyName) );
+        }
+
+        //获取系统使用的java版本
+        String javaVersion = System.getProperty("java.version");
+        System.out.println(javaVersion);
+
+    }
+
+}
