a1ee574aedb6303b443a8867863977c05ceb2585
今天幸福不在家 <zhujwmail@163.com>
2022-05-16 12:40:29 +0800

    泛型补充

--- base/src/main/java/com/zjw/generic/GenericTest.java
+++ base/src/main/java/com/zjw/generic/GenericTest.java
@@ -8,6 +8,8 @@ public class GenericTest {
     public static void main(String[] args) {
         String middle = ArrayAlg.getMiddle("John", "Q.", "a");
         System.out.println(middle);
+        Integer middle1 = ArrayAlg.getMiddle(1, 45, 6, 2, 34);
+        System.out.println(middle1);
 
     }
 }
