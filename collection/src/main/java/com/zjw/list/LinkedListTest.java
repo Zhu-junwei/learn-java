a4c64f9ef487deeaceb5e8b4c6f8925a3c158fd5
今天幸福不在家 <zhujwmail@163.com>
2022-05-17 01:13:59 +0800

    ArrayList和LinkedList的使用

---
+++ collection/src/main/java/com/zjw/list/LinkedListTest.java
@@ -0,0 +1,62 @@
+package com.zjw.list;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ *  原先文章：<a>https://www.cnblogs.com/zjw-blog/p/13727231.html</a>
+ * @author 朱俊伟
+ * @date 2022/05/17 0:02
+ */
+public class LinkedListTest
+{
+    public static void main(String[] args)
+    {
+        List<String> a = new LinkedList<>();
+        a.add("Amy");
+        a.add("Carl");
+        a.add("Erica");
+
+        List<String> b = new LinkedList<>();
+        b.add("Bob");
+        b.add("Doug");
+        b.add("Frances");
+        b.add("Gloria");
+
+        // merge the words from b into a
+
+        ListIterator<String> aIter = a.listIterator();
+        Iterator<String> bIter = b.iterator();
+
+        while (bIter.hasNext())
+        {
+            if (aIter.hasNext()) aIter.next();
+            aIter.add(bIter.next());
+        }
+
+        System.out.println(a);
+
+        // remove every second word from b
+
+        bIter = b.iterator();
+        while (bIter.hasNext())
+        {
+            bIter.next(); // skip one element
+            if (bIter.hasNext())
+            {
+                bIter.next(); // skip next element
+                bIter.remove(); // remove that element
+            }
+        }
+
+        System.out.println(b);
+
+        // bulk operation: remove all words in b from a
+
+        a.removeAll(b);
+
+        System.out.println(a);
+    }
+}
--- collection/src/main/java/com/zjw/list/ListTest.java
+++ collection/src/main/java/com/zjw/list/ListTest.java
@@ -1,14 +1,122 @@
 package com.zjw.list;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
+ * 原先文章：<a>https://www.cnblogs.com/zjw-blog/p/13727231.html</a>
  * @author 朱俊伟
  * @date 2022/04/20 14:47
  */
 public class ListTest {
     public static void main(String[] args) {
-        ArrayList<String> files = new ArrayList<>();
+
+        List<String> files = new ArrayList<>();
         files.add("123");
+        files.add("456");
+        files.add("456");
+//        testToArray(files);
+//        testContains(files,"123");
+//        testContainsAll(files);
+//        testGet(files,1);
+//        testForEach(files);
+//        testIterator(files);
+//        testIteratorRemove(files,"456");
+//        testListRemove(files,"456");
+    }
+
+    /**
+     * 集合转为数组
+     * @param list 集合
+     */
+    public static void testToArray(List<String> list){
+        Object[] objects = list.toArray();
+        System.out.println(Arrays.toString(objects));
+    }
+
+    /**
+     * 集合中是否包含某些元素
+     * @param list 集合
+     * @param o 元素
+     */
+    public static void testContains(List<String> list,String o){
+        boolean contains = list.contains(o);
+        System.out.println(contains);
+    }
+
+    /**
+     * containsAll方法
+     * 集合中是否包含另外一个集合中所有元素，包含返回true,有不包含的返回false
+     * @param list 集合
+     */
+    public static void testContainsAll(List<String> list){
+        //另外一个集合
+        List<String> containsList = new ArrayList<>();
+        containsList.add("123");
+        containsList.add("1");
+
+        boolean contains = list.containsAll(containsList);
+        System.out.println(contains);
+    }
+
+    /**
+     * 取得索引出处的元素，索引从0开始
+     * @param list 集合
+     * @param index 索引
+     */
+    public static void testGet(List<String> list,int index){
+        String s = list.get(index);
+        System.out.println(s);
+    }
+
+    /**
+     * 使用foreach遍历集合，
+     * <b>不能在遍历的时候删除元素</b>
+     * @param list 集合
+     */
+    public static void testForEach(List<String> list){
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+
+    /**
+     * 使用迭代器Iterator遍历集合
+     * @param list 集合
+     */
+    public static void testIterator(List<String> list){
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+    }
+
+    /**
+     * 使用迭代器Iterator遍历集合,在遍历的过程中对集合修改
+     * @param list 集合
+     */
+    public static void testIteratorRemove(List<String> list,String remove){
+        System.out.println(list);
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            String next = iterator.next();
+            if (remove.equals(next)){
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
+    }
+
+    /**
+     * 使用removeIf方法移除符合条件的元素
+     * @param list 集合
+     * @param filter 待移除的元素
+     */
+    public static void testListRemove(List<String> list,String filter){
+        System.out.println(list);
+        list.removeIf(filter::equals);
+        System.out.println(list);
     }
 }
