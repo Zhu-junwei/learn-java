0539769e08b9ac1efa54518934c32a7707b5899f
今天幸福不在家 <zhujwmail@163.com>
2023-05-05 16:21:28 +0800

    测试Math.min方法

---
+++ base/src/main/java/com/zjw/number/MathTest.java
@@ -0,0 +1,15 @@
+package com.zjw.number;
+
+import org.junit.Test;
+
+/**
+ * @author zjw
+ * @date 2023/05/05 15:45
+ */
+public class MathTest {
+
+    @Test
+    public void testMathMin(){
+        System.out.println(Math.min(1,2));//1
+    }
+}
