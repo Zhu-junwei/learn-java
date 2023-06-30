dd371b83def2d22dea52b5ec53f53423ef8fe32a
今天幸福不在家 <zhujwmail@163.com>
2023-06-30 17:54:27 +0800

    Period类使用

---
+++ base/src/main/java/com/zjw/time/PeriodTest.java
@@ -0,0 +1,28 @@
+package com.zjw.time;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+
+/**
+ * @author zjw
+ * @date 2023/06/30 17:40
+ */
+public class PeriodTest {
+    @Test
+    public void getMonths() {
+        // 创建两个 LocalDate 对象表示日期
+        LocalDate date1 = LocalDate.of(2022, Month.JANUARY, 1);
+        LocalDate date2 = LocalDate.of(2023, Month.MARCH, 15);
+
+        // 计算两个日期之间的月份差值
+        Period period = Period.between(date1, date2);
+
+        // 获取月份差值
+        int monthDiff = period.getMonths();
+
+        System.out.println("相差的月份: " + monthDiff);
+    }
+}
