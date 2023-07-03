aa6d5ebfd867e4c21c857bf9ba3728139467e9ea
今天幸福不在家 <zhujwmail@163.com>
2023-07-03 19:56:59 +0800

    BigDecimal转double

--- base/src/main/java/com/zjw/baseType/BaseTypeDemo.java
+++ base/src/main/java/com/zjw/baseType/BaseTypeDemo.java
@@ -2,8 +2,11 @@ package com.zjw.baseType;
 
 import org.junit.Test;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
- * 字符的使用
+ * 基本数据类型
  * @author 朱俊伟
  * @date 2022/02/07 20:58
  */
@@ -19,5 +22,19 @@ public class BaseTypeDemo {
         //۞ 使用UniCode编码表示字符
         char c2 = '\u06de';
         System.out.println(c2);
+    }
+
+    /**
+     * 测试BigDecimal转double
+     */
+    @Test
+    public void testdouble(){
+        double d1 = 1.23;
+        System.out.println(d1);
+        System.out.println(1.23 + 1.45);
+        BigDecimal  bigDecimal = new BigDecimal(1.23);
+        BigDecimal bigDecimal1 = new BigDecimal(1.45);
+        System.out.println(bigDecimal.add(bigDecimal1).setScale(2, RoundingMode.HALF_UP).doubleValue());
+        System.out.println(2.68);
     }
 }
