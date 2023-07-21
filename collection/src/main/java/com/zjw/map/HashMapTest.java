29eb805035cfc8be35bbc4e2627f987d89e1c2b6
今天幸福不在家 <zhujwmail@163.com>
2023-07-21 19:17:41 +0800

    测试map的containsKey方法

--- collection/src/main/java/com/zjw/map/HashMapTest.java
+++ collection/src/main/java/com/zjw/map/HashMapTest.java
@@ -1,5 +1,7 @@
 package com.zjw.map;
 
+import org.junit.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,5 +19,19 @@ public class HashMapTest {
         System.out.println(map.containsKey("1"));//true
         System.out.println(map.containsKey("2"));//false
 
+    }
+
+    @Test
+    public void testContainsKey(){
+        Map<Long , String> map = new HashMap<>();
+        map.put(Long.parseLong("1"),"a");
+        map.put(Long.parseLong("123456"),"b");
+        map.put(Long.parseLong("123456"),"c");
+        System.out.println(map);//{1=a}
+        System.out.println(map.get(Long.parseLong("1")));//a
+        System.out.println(map.get(Long.parseLong("123456")));//c
+        System.out.println(map.get(Long.parseLong("123457")));//null
+        System.out.println(map.containsKey(Long.parseLong("123456")));//true
+        System.out.println(map.containsKey(Long.parseLong("123457")));//false
     }
 }
