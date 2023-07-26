19dc4491a1c16bef6a65e5bd9be46a2f8e0a369a
今天幸福不在家 <zhujwmail@163.com>
2023-07-26 18:38:47 +0800

    数组初始化问题

---
+++ base/src/main/java/com/zjw/arrays/StringArrayTest.java
@@ -0,0 +1,26 @@
+package com.zjw.arrays;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+/**
+ * @author zjw
+ * @date 2023/07/26 13:38
+ */
+public class StringArrayTest {
+
+    /**
+     * 根据条件初始化数组
+     */
+    @Test
+    public void testStringArray(){
+        String[] strings = null;
+        if (2<1){
+            strings = new String[]{"1", "2"};
+        } else {
+            strings = new String[]{"1", "2", "3"};
+        }
+        System.out.println(Arrays.toString(strings));
+    }
+}
