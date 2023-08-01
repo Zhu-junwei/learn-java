d928d408927e8a5d47d0f61a7372ad8cab413881
今天幸福不在家 <zhujwmail@163.com>
2023-08-01 20:17:15 +0800

    Long类型判断相等

--- base/src/main/java/com/zjw/number/LongTest.java
+++ base/src/main/java/com/zjw/number/LongTest.java
@@ -18,5 +18,24 @@ public class LongTest {
         Long b = 1000L;
         System.out.println(a == b);//false
         System.out.println(a.equals(b));//true
+
+        System.out.println("**********");
+        Long c1 = 100L;
+        Long c2 = 100L;
+        System.out.println(c1 == c2);//true
+        System.out.println(c1.equals(c2));//true
+
+        System.out.println("127**********");
+        Long d1 = 127L;
+        Long d2 = 127L;
+        System.out.println(d1 == d2);//true
+        System.out.println(d1.equals(d2));//true
+
+        System.out.println("128**********");
+        Long e1 = 128L;
+        Long e2 = 128L;
+        System.out.println(e1 == e2);//false
+        System.out.println(e1.equals(e2));//true
+
     }
 }
