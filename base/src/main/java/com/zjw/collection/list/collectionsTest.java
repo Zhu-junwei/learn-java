4ae9d6e74729d30f3121403e8ba8804ebf5b4e4d
今天幸福不在家 <zhujwmail@163.com>
2023-12-22 16:23:55 +0800

    proxy

---
+++ base/src/main/java/com/zjw/collection/list/collectionsTest.java
@@ -0,0 +1,61 @@
+package com.zjw.collection.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Collections工具类的使用
+ */
+public class collectionsTest {
+    public static void main(String[] args) {
+ 
+        List<Integer> list = new ArrayList<>();
+        list.add(23);
+        list.add(4);
+        list.add(45);
+        list.add(76);
+        list.add(12);
+        System.out.println("原始集合："+list);
+ 
+        //排序
+        Collections.sort(list); //升序
+        System.out.println("排序后："+list);
+ 
+        //排序后可以二分查找
+        int index = Collections.binarySearch(list, 45);//查找位置，没有找到结果为负
+        System.out.println(index);
+ 
+        //拷贝
+        //需要拷贝到的目的集合要先有数据，才能拷贝
+        List<Integer> dest = new ArrayList<>();
+        for (int i = 0; i < list.size(); i++) {
+            dest.add(0);
+        }
+        Collections.copy(dest,list);
+        System.out.println("拷贝后的集合："+dest);
+ 
+        //反转
+        Collections.reverse(list);
+        System.out.println("反转后："+ list);
+ 
+        //打乱
+        Collections.shuffle(list);
+        System.out.println("打乱后："+ list);
+        Collections.shuffle(list);
+        System.out.println("打乱后："+ list);
+ 
+        //最大，最小
+        Integer max = Collections.max(list);
+        System.out.println("最大："+max);
+        Integer min = Collections.min(list);
+        System.out.println("最小："+min);
+ 
+        //使用指定元素替换集合中的所有元素
+        Collections.fill(list,0);
+        System.out.println("元素填充后："+list);
+ 
+ 
+    }
+}
+ 
--- base/src/main/java/com/zjw/proxy/readme.md
+++ base/src/main/java/com/zjw/proxy/readme.md
@@ -4,3 +4,11 @@ proxy2
 
 proxy3
     cglib动态代理，基于子类
+
+在Java升级到17后cglib不能使用了，报了如下的错误。
+
+```
+Exception in thread "main" java.lang.ExceptionInInitializerError
+	at com.zjw.cglib.Client.main(Client.java:40)
+Caused by: net.sf.cglib.core.CodeGenerationException: java.lang.reflect.InaccessibleObjectException-->Unable to make protected final java.lang.Class java.lang.ClassLoader.defineClass(java.lang.String,byte[],int,int,java.security.ProtectionDomain) throws java.lang.ClassFormatError accessible: module java.base does not "opens java.lang" to unnamed module @722c41f4
+	at net.sf
