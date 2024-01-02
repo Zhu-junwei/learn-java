f8f4f98edccdc7ba2a40a153880f648257054c26
今天幸福不在家 <zhujwmail@163.com>
2024-01-02 21:05:20 +0800

    Add jdk7 try resources test

---
+++ base/src/main/java/com/feature/java7/TryResourcesTest.java
@@ -0,0 +1,53 @@
+package com.feature.java7;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ * @author 朱俊伟
+ * @since 2024/01/02 18:47
+ */
+public class TryResourcesTest {
+
+    public static void main(String[] args) {
+        //java7之前的写法，需要在finally手动关闭资源
+        tryFinally();
+        //java7之后的写法
+        tryResources();
+    }
+
+    /**
+     * java7之前的写法，需要在finally手动关闭资源
+     */
+    public static void tryFinally() {
+        //读取文本文件的内容
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(new File("D://read.txt"));
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (scanner != null) {
+                scanner.close();
+            }
+        }
+    }
+
+    /**
+     * java7之后的写法，将需要释放的资源写到try里面，会自动释放
+     */
+    public static void tryResources() {
+        //读取文本文件的内容
+        try (Scanner scanner = new Scanner(new File("D://read.txt"))) {
+            while (scanner.hasNext()) {
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
