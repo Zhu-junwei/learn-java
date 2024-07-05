0312b6cf4c350634faf2ff643f197ce94c3e15b2
今天幸福不在家 <zhujwmail@163.com>
2024-07-05 12:26:08 +0800

    Add template-method design pattern

---
+++ design-pattern/src/main/java/com/zjw/templateMethod/TemplatePatternDemo.java
@@ -0,0 +1,16 @@
+package com.zjw.templateMethod;
+
+/**
+ * 客户端代码使用模板方法
+ */
+public class TemplatePatternDemo {
+    public static void main(String[] args) {
+        Game game = new Cricket();
+        game.play(); // 输出 Cricket 游戏流程
+
+        System.out.println();
+
+        game = new Football();
+        game.play(); // 输出 Football 游戏流程
+    }
+}
