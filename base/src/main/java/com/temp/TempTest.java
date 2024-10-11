eed99296aa2ac54d066ac418001d21e2eaeab972
今天幸福不在家 <zhujwmail@163.com>
2024-10-11 23:10:18 +0800

    Remove redundant tests

--- base/src/main/java/com/temp/TempTest.java
+++ base/src/main/java/com/temp/TempTest.java
@@ -1,10 +1,5 @@
 package com.temp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Random;
-import java.util.stream.IntStream;
-
 /**
  * @author 朱俊伟
  * @since 2024/03/20 22:11
@@ -12,16 +7,6 @@ import java.util.stream.IntStream;
 public class TempTest {
 
     public static void main(String[] args){
-        RandomList<String> randomList = new RandomList<>();
-        randomList.addAll(Arrays.asList("a b c d e f g h i".split(" ")));
-        IntStream.range(0, randomList.size()).forEach(i -> System.out.println(randomList.select()));
-    }
-}
 
-class RandomList<T> extends ArrayList<T> {
-    private Random random = new Random();
-
-    public T select() {
-        return get(random.nextInt(size()));
     }
 }
